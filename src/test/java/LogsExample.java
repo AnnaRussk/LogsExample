64209ddad80c1cb23a45b9c8c56817d14ac508f4
”AnnaRussk” <anna.russkih13@gmail.com>
2024-12-31 09:55:35 +0700

    Warn added, updating logger's version

--- src/test/java/LogsExample.java
+++ src/test/java/LogsExample.java
@@ -6,11 +6,22 @@ public class LogsExample {
   private static final Logger logger = LogManager.getLogger(LogsExample.class);
 
   public static void main(String[] args) {
-    logger.info("Запуск логгера");
-    logger.error("Пример ошибки");
-    logger.debug("Отладка");
+    logger.info("Программа запущена");
 
-    logger.warn("Какое-нибудь предупреждение");
+    int[] numbers = {1, 2, 3, 4, 5};
+
+    try {
+      logger.debug("Начинаем обход массива длиной {}", numbers.length);
+
+      // Цикл с ошибкой: пытаемся выйти за пределы массива
+      for (int i = 0; i <= numbers.length; i++) { // Ошибка: условие <= вместо <
+        logger.info("Элемент [{}]: {}", i, numbers[i]);
+      }
+    } catch (ArrayIndexOutOfBoundsException e) {
+      logger.error("Ошибка доступа к массиву: {}", e.getMessage(), e);
+    }
+
+    logger.info("Программа завершена");
   }
 
 }
