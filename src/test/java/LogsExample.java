ca98ef76ad891324555c3bba380cda236a054f77
”AnnaRussk” <anna.russkih13@gmail.com>
2024-12-31 09:30:41 +0700

    Logs added

---
+++ src/test/java/LogsExample.java
@@ -0,0 +1,14 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogsExample {
+
+  private static final Logger logger = LogManager.getLogger(LogsExample.class);
+
+  public static void main(String[] args) {
+    logger.info("Запуск логгера");
+    logger.error("Пример ошибки");
+    logger.debug("Отладка");
+  }
+
+}
